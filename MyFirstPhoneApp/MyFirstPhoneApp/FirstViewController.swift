7b0ccfbe51d55599a0d6270de2397fb5189b875a
Stanley Sun <Stanley.Lei.Sun@gmail.com>
2015-08-30 16:19:30 +0800

    First check-in for my iPhone play ground app.

---
+++ MyFirstPhoneApp/MyFirstPhoneApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MyFirstPhoneApp
+//
+//  Created by Stanley Sun on 15/8/30.
+//  Copyright (c) 2015年 Stanley Sun. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
