89616d3cb12e0031f58896726c4356d9e2a0678f
Stanley Sun (STC) <stansun@microsoft.com>
2018-01-23 09:49:03 +0800

    E2E works with return echo

--- RobertLog/AzureWeb/main.py
+++ RobertLog/AzureWeb/main.py
@@ -1,12 +1,27 @@
 from flask import Flask
+#from flask import render_template
+from weixinInterface import WeixinInterface
+from xml import etree
+import xml.etree.ElementTree as ET
+
 app = Flask(__name__)
 
-from weixinInterface import WeixinInterface
 weixin = WeixinInterface()
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  str_xml =  "<xml><ToUserName><![CDATA[fromUser]]></ToUserName>" +\
+        "<FromUserName><![CDATA[toUser]]></FromUserName>" +\
+        "<CreateTime>123445</CreateTime>" +\
+        "<MsgType><![CDATA[text]]></MsgType>"+\
+        "<Content><![CDATA[content]]></Content>"+\
+        "</xml>"
+  xml = ET.fromstring(str_xml)
+  content=xml.find("Content").text
+  msgType=xml.find("MsgType").text
+  fromUser=xml.find("FromUserName").text
+  toUser=xml.find("ToUserName").text
+  return content
 
 @app.route('/weixin', methods=['GET'])
 def weixin_get():
