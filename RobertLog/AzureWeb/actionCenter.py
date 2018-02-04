06e3171bf93527b3ae0ef3fb12ab2b9a5c06e613
Stanley Sun <stansun@microsoft.com>
2018-02-04 22:45:23 +0800

    fix sleep bugs

--- RobertLog/AzureWeb/actionCenter.py
+++ RobertLog/AzureWeb/actionCenter.py
@@ -87,8 +87,8 @@ class ActionCenter:
                 sleep_t = ect.extract_time(pre_content)
                 if sleep_t is None or len(sleep_t) <= 0:
                     sleep_t = sleep.TimeStamp
-                action.Detail = "从{0}到{1}，睡了{2}分钟".format(sleep_t.strftime( "%H:%M"), \
-                    action.TimeStamp.strftime( "%H:%M"), int((action.TimeStamp - sleep_t).total_seconds()/60)
+                action.Detail = "从{0}到{1}，睡了{2:.2f}小时".format(sleep_t.strftime( "%H:%M"), \
+                    action.TimeStamp.strftime( "%H:%M"), int((action.TimeStamp - sleep_t).total_seconds()/3600))
         else:
             action.Type = ActionType.UnKnown
         
