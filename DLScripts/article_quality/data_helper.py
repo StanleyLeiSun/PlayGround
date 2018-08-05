9c9641a77012dba8e54b448bc0f737285702ae94
Stanley Sun <Stanley.Lei.Sun@gmail.com>
2018-08-05 22:28:49 +0800

    add setup file for model

--- DLScripts/article_quality/data_helper.py
+++ DLScripts/article_quality/data_helper.py
@@ -1,22 +1,21 @@
 """
-author:stansun
+description: this file helps to load raw file and gennerate batch x,y
+author:luchi
+date:22/11/2016
 """
 import numpy as np
 import pandas as pd
-import pickle as p
-import sklearn.feature_extraction.text as skyfe
+import pickle as pkl
 
-
-data_root = '/home/stansun/data/'
 
 def load_data(max_len,batch_size,n_words=20000,valid_portion=0.1,sort_by_len=True):
     
-    train_set_x, train_set_y = load_dataset_file()
+    train_set_x, train_set_y, test_set = load_dataset_file()
 
     #train_set length
     n_samples= len(train_set_x)
     #shuffle and generate train and valid dataset
-    sidx = range(n_samples)
+    sidx = (n_samples)
     n_train = int(np.round(n_samples * (1. - valid_portion)))
     valid_set_x = [train_set_x[s] for s in sidx[n_train:]]
     valid_set_y = [train_set_y[s] for s in sidx[n_train:]]
@@ -32,7 +31,7 @@ def load_data(max_len,batch_size,n_words=20000,valid_portion=0.1,sort_by_len=Tru
     def remove_unk(x):
         return [[1 if w >= n_words else w for w in sen] for sen in x]
 
-    test_set_x, test_set_y = train_set_x, train_set_y 
+    test_set_x, test_set_y = test_set
     valid_set_x, valid_set_y = valid_set
     train_set_x, train_set_y = train_set
 
@@ -59,23 +58,23 @@ def load_data(max_len,batch_size,n_words=20000,valid_portion=0.1,sort_by_len=Tru
         train_set_x = [train_set_x[i] for i in sorted_index]
         train_set_y = [train_set_y[i] for i in sorted_index]
 
-        train_set=(train_set_x,train_set_y)
-        valid_set=(valid_set_x,valid_set_y)
-        test_set=(test_set_x,test_set_y)
+    train_set=(train_set_x,train_set_y)
+    valid_set=(valid_set_x,valid_set_y)
+    test_set=(test_set_x,test_set_y)
 
 
-        new_train_set_x=np.zeros([len(train_set[0]),max_len])
-        new_train_set_y=np.zeros(len(train_set[0]))
+    new_train_set_x=np.zeros([len(train_set[0]),max_len])
+    new_train_set_y=np.zeros(len(train_set[0]))
 
-        new_valid_set_x=np.zeros([len(valid_set[0]),max_len])
-        new_valid_set_y=np.zeros(len(valid_set[0]))
+    new_valid_set_x=np.zeros([len(valid_set[0]),max_len])
+    new_valid_set_y=np.zeros(len(valid_set[0]))
 
-        new_test_set_x=np.zeros([len(test_set[0]),max_len])
-        new_test_set_y=np.zeros(len(test_set[0]))
+    new_test_set_x=np.zeros([len(test_set[0]),max_len])
+    new_test_set_y=np.zeros(len(test_set[0]))
 
-        mask_train_x=np.zeros([max_len,len(train_set[0])])
-        mask_test_x=np.zeros([max_len,len(test_set[0])])
-        mask_valid_x=np.zeros([max_len,len(valid_set[0])])
+    mask_train_x=np.zeros([max_len,len(train_set[0])])
+    mask_test_x=np.zeros([max_len,len(test_set[0])])
+    mask_valid_x=np.zeros([max_len,len(valid_set[0])])
 
 
 
@@ -102,17 +101,40 @@ def load_data(max_len,batch_size,n_words=20000,valid_portion=0.1,sort_by_len=Tru
     return train_set,valid_set,test_set
 
 def load_dataset_file():
-    dataset_path= data_root + 'tencent_quality.pickle'
+    dataset_path='data/subj0.pkl'
     f=open(dataset_path,'rb')
-    print ('load data from %s'%dataset_path)
-    #train_set = np.array(pkl.load(f))
-    #test_set = np.array(pkl.load(f))
-    dataset = p.load(f)
+    print ('load data from %s',dataset_path)
+    train_set = np.array(pkl.load(f))
+    test_set = np.array(pkl.load(f))
     f.close()
 
-    train_set_x = dataset['x']
-    train_set_y = dataset['y']
-    return train_set_x, train_set_y
+    train_set_x,train_set_y = train_set
+    return train_set_x, train_set_y, test_set
+
+def build_dict(d, sentence):
+    for w in sentence:
+        if w in dict:
+            d[w] +=1
+        else:
+            d[w] = 1
+
+def build_embedding(titles):
+    dic = {}
+    for s in titles:
+        build_dict(dic, s)
+    
+
+
+def load_rawdata_file():
+    dataset_path='../data/article.csv'
+    article = pd.read_table(dataset_path,  error_bad_lines=False,)
+    article = article[['title','quality']].drop_duplicates()
+    train_set_x = np.array(article['title'])
+    
+    train_set_y = np.array(article[['quality']])
+
+    test_set = {train_set_x, train_set_y}
+    return train_set_x, train_set_y, test_set
 
 #return batch dataset
 def batch_iter(data,batch_size):
@@ -136,100 +158,5 @@ def batch_iter(data,batch_size):
         #     print return_mask_x
         #     import sys
         #     sys.exit(0)
-        yield [return_x,return_y,return_mask_x]
+        yield (return_x,return_y,return_mask_x)
 
-
-
-#region vectorlize doc
-
-def save_embedded(mapping, training_x, training_y, version):
-    data = {'mapping':mapping, 'x': training_x, 'y': training_y}
-    with open(data_root+version+'.pickle', 'wb') as f:
-        p.dump(data, f)
-
-def build_dict(d, sentence):
-    for w in sentence:
-        if w in d:
-            d[w] +=1
-        else:
-            d[w] = 1
-
-def build_embedding_map(titles, vacabulary_size):
-    dic = {}
-    for s in titles:
-        build_dict(dic, s)
-
-    sortdict = sorted(dic.items(), key = lambda x: x[1], reverse=True) 
-    print("Total vacabulary: %d, reduce to: %d"%(len(sortdict), vacabulary_size))
-    ret_map = {}
-    for i in range(0, vacabulary_size):
-        ret_map[sortdict[i][0]] = vacabulary_size - i
-     
-    return ret_map
-
-def encoding_sentence(sentence, mapping ):
-    return [mapping.get(w,0) for w in sentence ]
-
-def vector_rawdata_file():
-    dataset_path='~/data/tencent-dump.csv'
-    article = pd.read_table(dataset_path,  error_bad_lines=False)
-    article = article[['title','quality']].drop_duplicates().dropna()
-    train_set_x = np.array(article['title'])
-    train_set_y = np.array(article[['quality']])
-
-    mapping = build_embedding_map(train_set_x, 5000)
-    train_set_x = [ encoding_sentence(s, mapping) for s in train_set_x]
-    save_embedded(mapping, train_set_x, train_set_y, 'tencent_quality')
-
-    return train_set_x, train_set_y
-
-def build_vectors(sentences, vacabulary_size):
-    vectorizer = skyfe.CountVectorizer()
-    trans = vectorizer.fit_transform(corpus)
-    fname = vectorizer.get_feature_names()
-    print (trans)
-    print (trans.toarray())
-    print(fname)
-
-    #enable tf-idf
-    transformer = skyfe.TfidfTransformer()
-    tfidf = transformer.fit_transform(trans)  
-    print(tfidf.toarray())
-    print(tfidf.get_feature_names())
-
-    #hashed
-    vectorizer2 = skyfe.HashingVectorizer(n_features = 6,norm = None)
-    trans = vectorizer2.fit_transform(corpus)
-    #fname = vectorizer2.get_feature_names()
-    print (trans.toarray())
-    #print (fname)
-
-#endregion
-
-def test():
-    train_set_x, train_set_y = load_rawdata_file()
-
-
-if __name__ == "__main__":
-    #vector_rawdata_file()
-    #test()
-
-    load_data(2000, 200)
-
-    titles = ["abcdefffh", "hello lsdm"]
-
-    corpus=["I come to China to travel", 
-        "This is a car polupar in China",          
-        "I love tea and Apple ",   
-        "The work is to write some papers in science"] 
-
-    #build_vectors(corpus, 200)
-
-    #map = build_embedding(titles, 6)
-    #f = open("mappingfile", 'wb') #二进制打开
-    #p.dump(map, f)
-    #f.close()
-
-    #print(map)
-
-    #print(encoding_sentence("hlala  ff", map))
---
+++ DLScripts/article_quality/setup.py
@@ -0,0 +1,2 @@
+import setuptools
+setuptools.setup(name='article-quality', version='1.0', packages=[''], author='stansun')
